2ec00a646997c825b29cc9df8eeba09451900008
WaffleWolf <m.coronado60@gmail.com>
2014-11-23 18:38:36 -0800

    Trying to set up a cup image. (not functional)

--- src/Java_Man/javaCup.java
+++ src/Java_Man/javaCup.java
@@ -1,6 +1,9 @@
 package Java_Man;
 
 import java.awt.*;
+import java.awt.image.BufferedImage;
+import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
 
 /**
  * Created by joshuamaroney on 11/19/14.
@@ -34,14 +37,13 @@ public class javaCup {
 
     public void tick(){
 
-
     }
-
 
 
     public void draw(Graphics g) {
         g.setColor(Color.RED);
         g.fillRect(xCoor * width, yCoor * height, width, height);
+        //g.drawImage(xCoor * width, yCoor * height, width, height,)
     }
 
    }
